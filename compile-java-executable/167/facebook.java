deb349e0df239ebb55e3528cdc768852d506aa20
TestGrid <info@testgrid.io>
2025-12-19 21:39:28 +0000

    Global Variable Updated

--- compile-java-executable/167/facebook.java
+++ compile-java-executable/167/facebook.java
@@ -1,3 +1,5 @@
 @Listeners(TestListener.class)
 public class facebook {
+
+    public static String var_test0 = "test0";
 }
---
+++ compile-java-executable/constants.java
@@ -0,0 +1,3 @@
+public class constants {
+	public static String var_test0 = "test0";
+}
